4537b3a5ebf3722a19c6b977f95c3f9e11f2e595
marcel <marcel@medialibs.com>
2023-10-11 14:03:31 +0300

    init projet

---
+++ app/(styleguide)/page.tsx
@@ -0,0 +1,13 @@
+import AccordionDemo from "./components/accordionDemo";
+import AlertDemo from "./components/alertDemo";
+import AlertDialogDemo from "./components/alertDialogDemo";
+
+export default function Styleguide() {
+  return (
+    <main className=" p-24">
+      <AccordionDemo />
+      <AlertDemo />
+      <AlertDialogDemo />
+    </main>
+  )
+}
